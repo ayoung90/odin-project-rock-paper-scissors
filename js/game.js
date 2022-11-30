581703560110e1c388e5e8f163f6a20ad0408ff3
ayoung90 <ayoung90@live.com.au>
2022-11-30 19:47:08 +1100

    Complete multiple game loop + output result + format with Prettier

--- js/game.js
+++ js/game.js
@@ -1,74 +1,110 @@
-
 /**
  * Hold all possible game moves + win and lose conditions
  */
 const validChoices = [
-    {
-        name: "rock"
-        , beats: "scissors"
-        , loses: "paper"
-    },
-    {
-        name: "paper"
-        , beats: "rock"
-        , loses: "scissors"
-    },
-    {
-        name: "scissors"
-        , beats: "paper"
-        , loses: "rock"
-    },
+  {
+    name: "rock",
+    beats: "scissors",
+    loses: "paper",
+  },
+  {
+    name: "paper",
+    beats: "rock",
+    loses: "scissors",
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+    loses: "rock",
+  },
+];
 
-]
-
-const playerWins = "You win!! :)";
-const computerWins = "Sorry, you lose!! :(";
-const tie = "Looks like a tie!";
+const playerWinResult = "You win!! :)";
+const computerWinResult = "Sorry, you lose!! :(";
+const tieResult = "Looks like a tie!";
 
 /**
  * 'Randomly' select a choice for the computer player
  * @returns String of either 'rock', 'paper' or 'scissors'
  */
 function getComputerChoice() {
-    let randInt = Math.floor(Math.random() * 3);
-    let choice = validChoices[randInt];
+  let randInt = Math.floor(Math.random() * 3);
+  let choice = validChoices[randInt];
 
-    return choice.name;
+  return choice.name;
 }
-function getPlayerChoice(){
-    let input;
-    let choiceObject;
-    while (true){
-        input = prompt("Please enter 'Rock, Paper or Scissors'");
-        input = input.toLowerCase();
-        choiceObject = validChoices.find((item) => item.name === input) // check it exists
-        if (choiceObject){
-            break;
-        }
-        alert(`Sorry, ${input} does not exist. Please enter 'Rock, Paper or Scissors'`);
-        
-    }
 
-    return choiceObject.name;
+/**
+ * Prompts input and validates that a correct choice has been made
+ * @returns String of either 'rock', 'paper' or 'scissors'
+ */
+function getPlayerChoice() {
+  let input;
+  let choiceObject;
+  while (true) {
+    input = prompt("Please enter 'Rock, Paper or Scissors'");
+    input = input.toLowerCase();
+    choiceObject = validChoices.find((item) => item.name === input); // check it exists
+    if (choiceObject) {
+      break;
+    }
+    alert(
+      `Sorry, ${input} does not exist. Please enter 'Rock, Paper or Scissors'`
+    );
+  }
+
+  return choiceObject.name;
 }
 
 /**
  * Take two player inputs and calculate the winner
  * Assumption is that the inputs are validated elsewhere
- * @param {*} playerSelection 
- * @param {*} computerSelection 
+ * @param {*} playerSelection
+ * @param {*} computerSelection
  * @returns String representing winner / loser
  */
 function playRound(playerSelection, computerSelection) {
-    let result;
-    let playerObject = validChoices.find((choice) => choice.name === playerSelection);
-    let computerObject = validChoices.find((choice) => choice.name === computerSelection);
+  let playerObject = validChoices.find(
+    (choice) => choice.name === playerSelection
+  );
+  let computerObject = validChoices.find(
+    (choice) => choice.name === computerSelection
+  );
 
-    if (playerObject == computerObject){
-        return result = tie;
-    }else if(playerObject.beats === computerObject.name){
-        return result = playerWins;
-    }else if (playerObject.loses === computerObject.name){
-        return result = computerWins;
+  if (playerObject == computerObject) {
+    return tieResult;
+  } else if (playerObject.beats === computerObject.name) {
+    return playerWinResult;
+  } else if (playerObject.loses === computerObject.name) {
+    return computerWinResult;
+  }
+}
+
+/**
+ * Play a series of Rock, Paper, Scissors games and output the result
+ * @param {*} rounds Number of rounds to play (Default = 5)
+ */
+function playGame(rounds = 5) {
+  let playerCounter = 0;
+  let computerCounter = 0;
+  let ties = 0;
+
+  for (i = 0; i < rounds; i++) {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    let result = playRound(playerChoice, computerChoice);
+
+    if (result === playerWinResult) {
+      playerCounter++;
+    } else if (result === computerWinResult) {
+      computerCounter++;
+    } else if (result === tieResult) {
+      ties++;
     }
+  }
+  console.log("GAME OVER ---- Rounds = " + rounds);
+  console.log("Player wins = " + playerCounter);
+  console.log("Computer wins = " + computerCounter);
+  console.log("Ties = " + ties);
 }
